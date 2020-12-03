ae2c528b0ef26bf04e2bb3472fee2fe9f23fa5cf
ArashRahmani <arash.rahmani.999@gmail.com>
2020-12-03 17:17:26 -0500

    fix issue in for loop

--- multivariable_polynomial_recursive_form.py
+++ multivariable_polynomial_recursive_form.py
@@ -3,7 +3,7 @@ from sympy import *
 from sympy.parsing.sympy_parser import standard_transformations,\
 implicit_multiplication_application
 from sympy import degree
-# node of a doubly linked list 
+# node of a doubly linked list
 from sympy import sympify
 from sympy import Symbol
 class node:
@@ -13,61 +13,14 @@ class node:
         self.right = right
         self.left = left
         self.exp = exp
-        self.CV = CV 
-# class create_list:       
-#     def __init__(self):    
-#     self.head = node(None);    
-#     self.tail = node(None);    
-#     self.head.next = self.tail;    
-#     self.tail.next = self.head;    
-       
-#     def add(self,exp,CV):    
-#     newnode = node(exp,CV);    
-#     #Checks if the list is empty.    
-#     if self.head.data is None:    
-#         #If list is empty, both head and tail would point to new node.    
-#         self.head = newnode;    
-#         self.tail = newnode;    
-#         newnode.next = self.head;    
-#     else:    
-#         #tail will point to new node.    
-#         self.tail.next = newnode;    
-#         #New node will become new tail.    
-#         self.tail = newnode;    
-#         #Since, it is circular linked list tail will point to head.    
-#         self.tail.next = self.head;    
-        
-#     #Displays all the nodes in the list    
-#     def display(self):    
-#     current = self.head;    
-#     if self.head is None:    
-#         print("List is empty");    
-#         return;    
-#     else:    
-#         print("nodes of the circular linked list: ");    
-#         #Prints each node by incrementing pointer.    
-#         print(current.data),    
-#         while(current.next != self.head):    
-#             current = current.next;    
-#             print(current.data),    
-        
-        
-# class CircularLinkedList:    
-#     cl = CreateList();    
-#     #Adds data to the list    
-#     cl.add(1);    
-#     cl.add(2);    
-#     cl.add(3);    
-#     cl.add(4);    
-#     #Displays all the nodes present in the list    
-#     cl.display();    
+        self.CV = CV    
 transformations = (standard_transformations +
     (implicit_multiplication_application,))
 P = "3 + x^2 + xyz(xy + y − z) + z^3(1 − 3x)"
 P = P.replace('^','**')
 P = P.replace('−','-')
 regular_expression = parse_expr(P, transformations=transformations)
-regular_expression = expand(regular_expression)
+regular_expression = expand(regular_expression).args
 for r in regular_expression:
     print(degree(r,gen = Symbol('z')))
     print(r.coeff(Symbol('z')**3))
